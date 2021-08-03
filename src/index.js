50337a2a419822ffeef69d615e09f46c54d80879
natakers <n.kersnovskaya@gmail.com>
2021-08-03 21:42:23 +0300

    fix: change code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 module.exports = function reverse (n) {
-	return n.toString().split("").reverse().join().parseInt();
+	return n.toString().split("").reverse().join("");
+	// .parseInt();
   
 }
+// reverse(12314);
